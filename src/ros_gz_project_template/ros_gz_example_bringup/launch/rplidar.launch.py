27608179a53f150408a45cf1fa44a7a85a9fa828
Mikael Trana <mikael.trana@gmail.com>
2024-02-27 21:19:12 +0100

    Add lidar launch file

---
+++ src/ros_gz_project_template/ros_gz_example_bringup/launch/rplidar.launch.py
@@ -0,0 +1,34 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[
+                {'serial_port': 'dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0'},
+                {'frame_id': 'lidar_link'},
+                {'Angel_compensate': True},
+                {'scan_mode': 'Standard'},
+                {'serial_baudrate': 115200},
+
+                
+            ]
+        )
+    ])
