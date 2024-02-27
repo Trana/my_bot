d9d6c2607e5648e056adc41eea2cd9302f33ce67
Mikael Trana <mikael.trana@gmail.com>
2024-02-27 21:43:54 +0100

    FIx serial path

--- src/ros_gz_project_template/ros_gz_example_bringup/launch/rplidar.launch.py
+++ src/ros_gz_project_template/ros_gz_example_bringup/launch/rplidar.launch.py
@@ -22,13 +22,13 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[
-                {'serial_port': 'dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0'},
+                {'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0'},
                 {'frame_id': 'lidar_link'},
-                {'Angel_compensate': True},
+                {'angel_compensate': True},
                 {'scan_mode': 'Standard'},
                 {'serial_baudrate': 115200},
 
                 
             ]
         )
-    ])
+    ])§s
