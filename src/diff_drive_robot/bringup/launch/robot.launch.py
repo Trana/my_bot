6762cb7878b35f51b3f173afea15144f6b2cdf94
Mikael Trana <mikael.trana@gmail.com>
2024-03-10 14:34:00 +0100

    Clean up folder structure and remove example files

---
+++ src/diff_drive_robot/bringup/launch/robot.launch.py
@@ -0,0 +1,74 @@
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Configure ROS nodes for launch
+
+    # Setup project paths
+    pkg_project_bringup = get_package_share_directory('bringup')
+    pkg_project_gazebo = get_package_share_directory('gazebo')
+    pkg_project_description = get_package_share_directory('description')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+
+    ## Robot state publisher
+    robot_state_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(pkg_project_bringup,'launch','robot_state_publisher.launch.py')])
+    )
+    
+    ## RViz
+    rviz = Node(
+       package='rviz2',
+       executable='rviz2',
+       arguments=['-d', os.path.join(pkg_project_bringup, 'config', 'diff_drive.rviz')],
+       condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    ## Ros2 Control
+    diff_drive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=["diff_drive_base_controller"]
+    )
+
+    joint_state_broadcast_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=["joint_state_broadcaster"]
+    )
+
+
+    ## Launch
+    return LaunchDescription([
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        robot_state_publisher,
+        rviz,
+        diff_drive_controller_spawner,
+        joint_state_broadcast_spawner
+    ])
