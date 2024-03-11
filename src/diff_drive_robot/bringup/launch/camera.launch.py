7a9e11807487ded4ee078df9bfc974a1337c5f0d
Mikael Trana <mikael.trana@gmail.com>
2024-03-11 16:00:17 +0100

    Fixa Camera launch file to correspond to URDF

--- src/diff_drive_robot/bringup/launch/camera.launch.py
+++ src/diff_drive_robot/bringup/launch/camera.launch.py
@@ -15,7 +15,8 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'image_size': [640,480],
-                'camera_frame_id': 'camera_link'
+                'time_per_frame': [1, 6],
+                'camera_frame_id': 'camera_link_optical'
                 }]
     )
     ])
